62cfdabac09a79e66453cfce59b375faae00996f
Juan <jennifer.cortes.gu@gmail.com>
2020-08-24 21:30:16 -0500

    creación de del controlador para destinations

--- routes/destinations.js
+++ routes/destinations.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const api = express.Router();
+const destinations = require('../controllers/destinations')
 
-api.post('/create', () => { });
-api.put('/update/:id', () => { });
-api.delete('/remove/:id', () => {});
-api.get('/getAll', () => {});
-api.get('/getOne/:id', () =>{});
+api.post('/destinations/create', destinations.create);
+api.put('/destinations/update/:id', () => {});
+api.delete('/destinations/remove/:id', () => {});
+api.get('/destinations/getAll', () => {});
+api.get('/destinations/getOne/:id', () =>{});
+
+module.exports = api
