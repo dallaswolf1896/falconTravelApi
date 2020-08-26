59ef1caaee5c5c8cc403c396f7ad23f7c04ef5d6
Sebastian Najar <sebas.anc@hotmail.com>
2020-08-25 20:27:27 -0500

    cambios en el nombre de contact

--- routes/contactUs.js
+++ routes/contactUs.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const api = express.Router();
 
-/*const contactUs = require('../controllers/contactUs.controller')
+const contactUs = require('../controllers/contactUs')
 
 api.post('/contactUs/create', contactUs.create);
 api.put('/contactUs/update/', () => {});
 api.delete('/contactUs/remove/:id', () => {});
 api.get('/contactUs/getAll',() => {});
-api.get('/contactUs/getOne/:id',() => {});*/
+api.get('/contactUs/getOne/:id',() => {});
 
 module.exports = api;
