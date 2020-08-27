f62526efdf56d5c35562fc748d940ff7ead06d81
Coli1428 <66651071+Coli1428@users.noreply.github.com>
2020-08-26 20:42:08 -0500

    Se agrego el metodo de modificar destinos

--- routes/destinations.js
+++ routes/destinations.js
@@ -3,7 +3,7 @@ const api = express.Router();
 const destinations = require('../controllers/destinations')
 
 api.post('/destinations/create', destinations.create);
-api.put('/destinations/update/:id', () => {});
+api.put('/destinations/update/:id', destinations.update );
 api.delete('/destinations/remove/:id', () => {});
 api.get('/destinations/getAll', () => {});
 api.get('/destinations/getOne/:id', () =>{});
