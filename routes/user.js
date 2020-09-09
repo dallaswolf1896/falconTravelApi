b665095b6fda6f6d4820355fe0965d7e63dedb6d
PaolaBit <paola.cuadros@bit.institute>
2020-09-08 19:02:28 -0500

    Se eliminó el auth al momento de actualizar el usuario.

--- routes/user.js
+++ routes/user.js
@@ -12,7 +12,7 @@ const auth = require('../middleware/auth')
  */
 
 api.post('/user/create', user.create); //Vamos a crear un usuario
-api.put('/user/update/:id', auth.isAuth, user.update); //Vamos a modificar un usuario
+api.put('/user/update/:id', user.update); //Vamos a modificar un usuario
 api.delete('/user/remove/:id', () => {}); //Vamos a eliminar un usuario
 api.get('/user/getAll', user.getAll); //Vamos a obtener todos los usuarios
 api.post('/user/login', user.login);
