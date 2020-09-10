dc6dc807134350420744b1980a5616cf49e6a9f7
davidmoreno1988 <car_nemesis@hotmail.com>
2020-09-09 21:56:18 -0500

    Creamos conexión de destinationOne

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -91,7 +91,7 @@ exports.getOne = (req, res) => {
         destination=>{
             res.send(destination)
         }
-    ).catch(
+    ).catch(error => {
         res.status(500).send({ message: error.message || 'Error de conexión' })
-    )
+    })
 }
